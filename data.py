ffca594462ee2cfe260c2e1a4ad1538a8b737954
sabijn <sabijn.perdijk@gmail.com>
2023-05-02 15:45:22 +0200

    jobscript lisa

--- data.py
+++ data.py
@@ -2,10 +2,7 @@
 Dataset: https://huggingface.co/datasets/cc100
 Based on: https://huggingface.co/docs/transformers/main/tasks/masked_language_modeling
 """
-
 from datasets import load_dataset
-import argparse
-from transformers import AutoTokenizer, AutoModelForMaskedLM, DataCollatorForLanguageModeling, Trainer, TrainingArguments
 
 def preprocess_function(examples):
     return tokenizer([" ".join(x) for x in examples["text"]])
@@ -29,24 +26,9 @@ def group_texts(examples):
     print(result)
     return result
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    
-    parser.add_argument('--languages', default='nl', type=str,
-                        help='Language to finetune on.')
-    parser.add_argument('--debug', default=100, type=int,
-                        help='Amount of samples to set up and debug the framework.')
-    parser.add_argument('--model', default="xlm-roberta-base", type=str,
-                       help='Pretrained model to use.')
-    parser.add_argument('--test_split', default=0.2, type=float, choices=range(0,1),
-                        help='Which percentage of the data to use for testing')
-    
-    args = parser.parse_args()
-    cc100 = load_dataset("cc100", lang=args.languages, split="train[:1000]")
+def prepare_dataset(args):
+    cc100 = load_dataset("cc100", lang=args.languages, split="train")
     cc100 = cc100.train_test_split(test_size=args.test_split)
-
-    tokenizer = AutoTokenizer.from_pretrained(args.model)
-    model = AutoModelForMaskedLM.from_pretrained(args.model)
 
     tokenized_cc100 = cc100.map(
             preprocess_function,
@@ -55,36 +37,7 @@ if __name__ == '__main__':
             remove_columns=cc100['train'].column_names
         )
 
-    lm_dataset = tokenized_cc100.map(group_texts, batched=True, num_proc=4)
+    dataset = tokenized_cc100.map(group_texts, batched=True, num_proc=4)
 
-    # Use end of sentence token as pad token
-    tokenizer.pad_token = tokenizer.eos_token
+    return dataset
 
-    # Padding and batching
-    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)
-
-    training_args = TrainingArguments(
-        output_dir=f"results_{args.languages}",
-        overwrite_output_dir = 'True',
-        evaluation_strategy="epoch",
-        learning_rate=2e-5,
-        num_train_epochs=10,
-        weight_decay=0.01,
-        push_to_hub=False,
-        save_total_limit = 2,
-        save_strategy = "no",
-        load_best_model_at_end=False
-    )
-
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=lm_dataset["train"],
-        eval_dataset=lm_dataset["test"],
-        data_collator=data_collator
-    )
-
-    trainer.train()
-
-    eval_results = trainer.evaluate()
-    print(eval_results)
---
+++ train.py
@@ -0,0 +1,59 @@
+import argparse
+from transformers import AutoTokenizer, AutoModelForMaskedLM, DataCollatorForLanguageModeling, Trainer, TrainingArguments
+
+from data import *
+
+def train(args, model, lm_dataset, data_collator):
+    training_args = TrainingArguments(
+        output_dir=f"results_{args.languages}",
+        overwrite_output_dir = 'True',
+        evaluation_strategy="epoch",
+        learning_rate=2e-5,
+        num_train_epochs=10,
+        weight_decay=0.01,
+        push_to_hub=False,
+        save_total_limit = 2,
+        save_strategy = "no",
+        load_best_model_at_end=False
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=lm_dataset["train"],
+        eval_dataset=lm_dataset["test"],
+        data_collator=data_collator
+    )
+
+    trainer.train()
+
+    eval_results = trainer.evaluate()
+    print(eval_results)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--languages', default='nl', type=str,
+                        help='Language to finetune on.')
+    parser.add_argument('--model', default="xlm-roberta-base", type=str,
+                       help='Pretrained model to use.')
+    parser.add_argument('--test_split', default=0.2, type=float, choices=range(0,1),
+                        help='Which percentage of the data to use for testing')
+    
+    args = parser.parse_args()
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
+    model = AutoModelForMaskedLM.from_pretrained(args.model)
+
+    # Prepare dataset
+    cc100_dataset = prepare_dataset(args)
+
+    # Use end of sentence token as pad token
+    tokenizer.pad_token = tokenizer.eos_token
+
+    # Padding and batching
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)
+
+    # Training
+    train(args, model, cc100_dataset, data_collator)
+
