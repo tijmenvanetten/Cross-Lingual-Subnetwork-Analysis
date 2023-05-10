4c92e4aa255f97785f06a41bd595274592c28652
Ilja van Ipenburg <ivanipenburg@gmail.com>
2023-05-10 14:17:57 +0200

    probably broken changes

--- preprocess_data.py
+++ preprocess_data.py
@@ -14,38 +14,38 @@ def preprocess_function(examples, tokenizer):
 def group_texts(examples):
     block_size = 128
 
-    print(examples.keys())
-    return None    
+    # concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    concatenated_examples = sum(examples, [])
 
-    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
-
-    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # total_length = len(concatenated_examples[list(examples.keys())[0]])
+    total_length = len(concatenated_examples)
     # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
     # customize this part to your needs.
     if total_length >= block_size:
         total_length = (total_length // block_size) * block_size
 
     # Split by chunks of block_size.
-    result = {
-        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
-        for k, t in concatenated_examples.items()
-    }
+    # result = {
+    #     k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+    #     for k, t in concatenated_examples.items()
+    # }
+    result = [concatenated_examples[i : i + block_size] for i in range(0, total_length, block_size)]
+
+
     result["labels"] = result["input_ids"].copy()
     return result
 
 
 def prepare_dataset(args, tokenizer):
-    cc100 = load_dataset("cc100", lang=args.languages, split="train")
+    cc100 = load_dataset("cc100", lang=args.languages)
 
-    
-    cc100 = cc100.train_test_split(test_size=0.2)
 
     tokenized_cc100 = cc100.map(
             preprocess_function,
             fn_kwargs={"tokenizer" : tokenizer},
             batched=True,
             num_proc=6,
-            remove_columns=cc100['train'].column_names
+            remove_columns=cc100.column_names
         )
 
     dataset = tokenized_cc100.map(group_texts, batched=True, num_proc=6)
