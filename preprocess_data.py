f408b8c35228e4bd5a689e5e59ce3a1ca8b5bbb5
Ilja van Ipenburg <ivanipenburg@gmail.com>
2023-05-10 14:03:49 +0200

    test without keys

--- preprocess_data.py
+++ preprocess_data.py
@@ -11,23 +11,43 @@ from transformers import AutoModelForMaskedLM, AutoTokenizer
 def preprocess_function(examples, tokenizer):
     return tokenizer(examples['text'])
 
+# def group_texts(examples):
+#     block_size = 128
+
+#     concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+
+#     total_length = len(concatenated_examples[list(examples.keys())[0]])
+#     # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
+#     # customize this part to your needs.
+#     if total_length >= block_size:
+#         total_length = (total_length // block_size) * block_size
+
+#     # Split by chunks of block_size.
+#     result = {
+#         k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+#         for k, t in concatenated_examples.items()
+#     }
+#     result["labels"] = result["input_ids"].copy()
+#     return result
+
 def group_texts(examples):
     block_size = 128
 
-    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    concatenated_examples = sum(examples, [])
 
-    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    total_length = len(concatenated_examples)
     # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
     # customize this part to your needs.
     if total_length >= block_size:
         total_length = (total_length // block_size) * block_size
     # Split by chunks of block_size.
+    num_chunks = total_length // block_size
     result = {
-        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
-        for k, t in concatenated_examples.items()
+        "input_ids": [concatenated_examples[i * block_size:(i + 1) * block_size]
+                      for i in range(num_chunks)],
+        "labels": [concatenated_examples[i * block_size:(i + 1) * block_size]
+                   for i in range(num_chunks)],
     }
-    result["labels"] = result["input_ids"].copy()
-    # print(result)
     return result
 
 def prepare_dataset(args, tokenizer):
